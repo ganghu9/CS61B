646ca83e28cfd713f80f3cfd0c0b8fdba096f5c4
Gang Hu <huyingguo2007@hotmail.com>
2024-01-01 18:06:58 -0500

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,22 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Gang Hu
  */
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
-    public static int nextNumber(int n) {
-        if (n  == 128) {
+    public static int nextNumber(int n)
+    {
+        // The last one
+        if (n == 1) {
             return 1;
-        } else if (n == 5) {
+        }
+        // Even number
+        else if (n % 2 == 0) {
+            return n/2;
+        }
+        // Odd number
+        else{
             return 3 * n + 1;
-        } else {
-            return n * 2;
         }
     }
 
